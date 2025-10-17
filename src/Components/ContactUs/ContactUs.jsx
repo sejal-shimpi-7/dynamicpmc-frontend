aeeeaa74c77ad338631a612ef5d08f6a1baf351f
Techoaholic07 <sejalsshimpi725@gmail.com>
2025-10-17 09:33:08 +0530

    updated iframe again

--- src/Components/ContactUs/ContactUs.jsx
+++ src/Components/ContactUs/ContactUs.jsx
@@ -123,19 +123,18 @@ const ContactUs = () => {
             </button>
           </form>
 
-          <div className="map-container">
-          {/* --- THIS IS THE CORRECTED IFRAME --- */}
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3781.442656515328!2d73.8080309148939!3d18.60002198736341!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2b9c7b9ffffff%3A0x53a992823071b933!2sBlueberry%20Biz!5e0!3m2!1sen!2sin!4v1668589250000!5m2!1sen!2sin"
-            width="100%"
-            height="100%"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-            title="Office Location"
-          ></iframe>
-        </div>
+         <div className="map-container">
+  <iframe 
+    src="http://googleusercontent.com/maps.google.com/7"
+    width="100%" 
+    height="100%" 
+    style={{ border: 0 }} 
+    allowFullScreen="" 
+    loading="lazy" 
+    referrerPolicy="no-referrer-when-downgrade"
+    title="Blueberry Biz Location">
+  </iframe>
+</div>
         </div>
       </div>
     </section>
